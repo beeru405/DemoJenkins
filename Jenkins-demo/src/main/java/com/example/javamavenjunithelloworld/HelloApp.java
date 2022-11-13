b79b2cf328b4150114c3aa553eb4132827fb6748
pavankumar0077 <dasarepavan007@gmail.com>
2022-11-13 08:33:05 +0530

    Added junit test cases

---
+++ Jenkins-demo/src/main/java/com/example/javamavenjunithelloworld/HelloApp.java
@@ -0,0 +1,10 @@
+package com.example.javamavenjunithelloworld;
+
+public class HelloApp {
+
+    public static void main(String[] args) {
+    	
+    	System.out.println("Hello Pavan");
+
+    }
+}
---
+++ Jenkins-demo/src/test/java/com/jenkinsdemoTest/HelloAppTest.java
@@ -0,0 +1,45 @@
+package com.jenkinsdemoTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+
+public class HelloAppTest {
+	
+	@BeforeAll
+	  static void beforeAll() {
+	    System.out.println("Before all Test Cases");
+	  }
+	
+	@BeforeEach
+	  void beforeEach() {
+	    System.out.println("Before each test method");
+	  }
+   
+	@Test
+	void testHello() {
+		assertEquals(true, true);
+	}
+	
+//	@Test
+//	void testHello1() {
+//		assertEquals("Hello Pavan", "Hello Pavan");
+//	}
+	
+	 @AfterEach
+	  void afterEach() {
+	    System.out.println("After each test method");
+	  }
+   
+	 @AfterAll
+	  static void afterAll() {
+	    System.out.println("After all test cases");
+	  }
+	
+}
